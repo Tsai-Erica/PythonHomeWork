20abfdb1b734019994564f1936d18752c9d61254
Tsai-Erica <102162911+Tsai-Erica@users.noreply.github.com>
2022-03-22 21:46:23 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 20:09:58 2022
+
+@author: USER
+"""
+
+for i in range(5,0,-1):
+    for a in range(i):
+        print(i,sep="",end="")
+    print()
---
+++ ex2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 21:18:50 2022
+
+@author: USER
+"""
+
+for a in range(2,10):
+    for b in range(1,10):
+        print(b,"*",a,"=",a*b,sep="",end="\t")
+
+    print()
